e59476c0afe0ad379c0d85818485427c4e6d2df5
Ted KIm <milkywood@gmail.com>
2025-04-10 17:34:57 +0900

    Add minus function with TC

--- main.cpp
+++ main.cpp
@@ -1,21 +1,40 @@
+#include <iostream>
 #include "gmock/gmock.h"
 
-class Gop
-{
+using std::cout;
+
+class Cal {
 public:
-  // 이곳에 코드 작성
-  int getGop(int a,int b) { return a * b;}
+	// 이곳에 코드 작성
+	int getMinus(int first, int second) {
+		int result = first - second;
+		if (first < second && result > first);
+		std::cout << "possibly underflow, please check the value range of operand type.\n";
+		return result;
+	}
 };
 
 // 테스트 케이스 작성
-TEST(Gop, test)
-{
-  Gop gop;
-  EXPECT_EQ(gop.getGop(2,4), 8);
+TEST(t1, t2) {
+	EXPECT_EQ(1, 1);
 }
 
-int main()
-{
-  ::testing::InitGoogleMock();
-  return RUN_ALL_TESTS();
+TEST(OpMinus_TS, PositiveTC1) {
+	Cal calTest;
+
+	int result = calTest.getMinus(10, 10);
+	int expected = 0;
+	EXPECT_EQ(expected, result);
+}
+TEST(OpMinus_TS, NegativeTC2) {
+	Cal calTest;
+
+	int result = calTest.getMinus(-2147483648, 2);
+
+	EXPECT_TRUE((result > 0));
+}
+
+int main() {
+	::testing::InitGoogleMock();
+	return RUN_ALL_TESTS();
 }
