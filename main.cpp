2ca6f8163ec1f1d46f636adf12857482c2dd2a2d
Chanwook Lee <cksdnr1@gmail.com>
2025-04-10 17:16:39 +0900

    feature sum commited

--- main.cpp
+++ main.cpp
@@ -3,9 +3,14 @@
 class Cal {
 public:
   // 이곳에 코드 작성
+int getSum(int a, int b) { return a + b; }
 };
 
 // 테스트 케이스 작성
+TEST(t1, sum) {
+  Cal cal;
+  EXPECT_EQ(3, cal.getSum(1, 2));
+}
 TEST(t1, t2) {
   EXPECT_EQ(1, 1);
 }
