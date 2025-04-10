40b1beea3a3dfe387cfa6a585657151166f447e6
dkqpeowkd <loststars@naver.com>
2025-04-10 17:30:57 +0900

    Update main.cpp
     getGop(a, b) : a * b 반환 구현

--- main.cpp
+++ main.cpp
@@ -1,16 +1,21 @@
 #include "gmock/gmock.h"
 
-class Cal {
+class Gop
+{
 public:
-  // 이곳에 코드 작성
+	// 이곳에 코드 작성
+	int getGop(int a, int b) { return a * b; }
 };
 
 // 테스트 케이스 작성
-TEST(t1, t2) {
-  EXPECT_EQ(1, 1);
+TEST(Gop, test)
+{
+	Gop gop;
+	EXPECT_EQ(gop.getGop(2, 4), 8);
 }
 
-int main() {
-  ::testing::InitGoogleMock();
-  return RUN_ALL_TESTS();
+int main()
+{
+	::testing::InitGoogleMock();
+	return RUN_ALL_TESTS();
 }
